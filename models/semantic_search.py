53f2a9bdc73c4960ec8a2b46b1799d6de2506685
Jisel Otavo <ladyotavo@gmail.com>
2024-02-27 00:08:27 -0500

    se agregan filtros a la busqueda semantica

---
+++ models/semantic_search.py
@@ -0,0 +1,33 @@
+from pinecone import Pinecone
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+pc = Pinecone(api_key='683338d7-6863-4d59-9ba5-6f4d422bab9d')
+index = pc.Index("relatoria-emebeddings")
+
+def search(json):
+    query_vector = model.encode(json['text']).tolist()
+    filter = []
+    if json['tipo']:
+        filter.append({
+            'Tipo': {'$eq': json['tipo']}
+        })
+    if json['anio']:
+        filter.append({
+            'anio': {'$eq': int(json['anio'])}
+        })
+    if json['fecha_inicio']:
+        filter.append({
+            'fecha_number': {'$gte': int(json['fecha_inicio'].replace('-', ''))}
+        })
+    if json['fecha_fin']:
+        filter.append({
+            'fecha_number': {'$lte': int(json['fecha_fin'].replace('-', ''))}
+        })
+    if len(filter) == 0:
+        filter = None
+    else:
+        filter = {'$and': filter}
+    print('filtros: ', filter)
+
+    return index.query(vector=query_vector, top_k = json['top_k'], include_metadata=True, filter=filter)
--- server.py
+++ server.py
@@ -2,30 +2,19 @@ import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from pinecone import Pinecone
-from sentence_transformers import SentenceTransformer, util
-from finetunned_model import chatbot
-
-# model
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-
-# embeddings
-pc = Pinecone(api_key='683338d7-6863-4d59-9ba5-6f4d422bab9d')
-index = pc.Index("relatoria-emebeddings")
+from models.semantic_search import search
+from models.finetunned_model import chatbot
 
 # flash app
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/search')
-def search_route():
-    query = request.args['query']
-    query_vector = model.encode(query).tolist()
-    response = index.query(vector=query_vector, top_k = 3, include_metadata=True)
+@app.route('/search', methods=['POST'])
+def search_route():    
     return jsonify(list(map(
         lambda item: item._data_store,
-        response['matches'],
+        search(request.json)['matches'],
     )))
 
 @app.route('/chatbot')
